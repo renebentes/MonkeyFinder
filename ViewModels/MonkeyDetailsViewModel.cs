ed3782bcb5f043ac7849305306ffd5ab1ca418ab
Rene Bentes Pinto <renebentes@yahoo.com.br>
2022-06-16 21:38:33 -0300

    feat: Visualizar localização no mapa

--- ViewModels/MonkeyDetailsViewModel.cs
+++ ViewModels/MonkeyDetailsViewModel.cs
@@ -5,4 +5,26 @@ public partial class MonkeyDetailsViewModel : BaseViewModel
 {
     [ObservableProperty]
     private Monkey monkey;
+
+    private readonly IMap _map;
+
+    public MonkeyDetailsViewModel(IMap map) => _map = map;
+
+    [RelayCommand]
+    private async Task OpenMapAsync()
+    {
+        try
+        {
+            await _map.OpenAsync(
+                new Location(Monkey.Latitude, Monkey.Longitude),
+                new MapLaunchOptions { Name = Monkey.Name, NavigationMode = NavigationMode.None }
+);
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("ERROR!", "Unable to open map!", "Ok");
+        }
+    }
 }
